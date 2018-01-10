33713aa5ad5214b643e24d9c559a740aaaf0368d
Daisuke Fujita <dtanshi45@gmail.com>
2018-01-10 11:25:10 +0900

    Extract necessary fields

--- rdm2bq.rb
+++ rdm2bq.rb
@@ -14,4 +14,28 @@ event = client.get_log_events(
   start_from_head: false, # latest log events are returned first
 ).events[0]
 
-p JSON.parse(event.message)
+metrics = JSON.parse(event.message)
+
+#
+# processList
+#   cpuUsedPc - The percentage of CPU used by the process.
+#   id - The identifier of the process.
+#   memoryUsedPc - The amount of memory used by the process, in kilobytes.
+#   name - The name of the process.
+#   parentID - The process identifier for the parent process of the process.
+#   rss - The amount of RAM allocated to the process, in kilobytes.
+#   tgid - The thread group identifier, which is a number representing the process ID to which a thread belongs. This identifier is used to group threads from the same process.
+#   vss - The amount of virtual memory allocated to the process, in kilobytes.
+#
+
+metrics["processList"].each do |process|
+  puts({
+    parent_pid: process["parentID"],
+    pid: process["id"],
+    name: process["name"].strip,
+    rss: process["rss"],
+    tgid: process["tgid"],
+    timestamp: metrics["timestamp"],
+    vss: process["vss"],
+  })
+end
